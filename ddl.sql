94cb3104b8154ea1149e1a0500c907e47cb1d939
Arthur Atangana <arthur.atangana@protonmail.com>
2024-03-11 16:31:27 -0400

    Initialize postgres database
    Add dml and ddl file for database creation

---
+++ ddl.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE University;
+
+CREATE TABLE Students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    enrollment_date DATE
+)
